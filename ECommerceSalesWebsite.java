26e8d762f2ca73ce85f94e47096e1bdf4d679f3a
Alok Roy Chatterjee <88044679+AlokRoyChatterjee@users.noreply.github.com>
2021-07-27 13:32:00 -0400

    Update and rename ECommerceSales.java to ECommerceSalesWebsite.java

--- ECommerceSalesWebsite.java
+++ ECommerceSalesWebsite.java
@@ -2,9 +2,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ECommerceSales {
+public class ECommerceSalesWebsite {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ECommerceSales.class, args);
+		SpringApplication.run(ECommerceSalesWebsite.class, args);
 	}
 }
